15b7f45fe5e42acdeb643548717d222d792173c4
dwestland <don@westland.net>
2023-06-16 11:38:31 -0700

    Add page, utils and layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,54 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+'use client'
+import {
+  useState
+} from 'react'
 
 export default function Home() {
+  const [query, setQuery] = useState('')
+  const [result, setResult] = useState('')
+  const [loading, setLoading] = useState(false)
+  async function createIndexAndEmbeddings() {
+    try {
+      const result = await fetch('/api/setup', {
+        method: "POST"
+      })
+      const json = await result.json()
+      console.log('result: ', json)
+    } catch (err) {
+      console.log('err:', err)
+    }
+  }
+  async function sendQuery() {
+    if (!query) return
+    setResult('')
+    setLoading(true)
+    try {
+      const result = await fetch('/api/read', {
+        method: "POST",
+        body: JSON.stringify(query)
+      })
+      const json = await result.json()
+      setResult(json.data)
+      setLoading(false)
+    } catch (err) {
+      console.log('err:', err)
+      setLoading(false)
+    }
+  }
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="flex flex-col items-center justify-between p-24">
+      <input className='text-black px-2 py-1' onChange={e => setQuery(e.target.value)} />
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full my-5" onClick={sendQuery}>Ask AI</button>
+      {
+        loading && <p>Asking AI ...</p>
+      }
+      {
+        result && <p>{result}</p>
+      }
+      { /* consider removing this button from the UI once the embeddings are created ... */}
+      <button onClick={createIndexAndEmbeddings} className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded my-10">
+        Create index and embeddings
+      </button>
     </main>
   )
 }
